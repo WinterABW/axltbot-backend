e23488b22caf3d3137719a5cc9440f5caddbda00
Victor Manuel Avila Tamayo <skarnanotherblackwinter@gmail.com>
2024-07-14 13:03:01 -0400

    done

--- bot.js
+++ bot.js
@@ -1,20 +1,36 @@
-const { Telegraf } = require("telegraf");
-const { message } = require("telegraf/filters");
-const bot = new Telegraf("6747056687:AAFU051yWsJqeawZs9uG4GJeN5p7o2vetOA");
+const { Telegraf, Markup } = require("telegraf");
+
+const app=process.env.APP
+const token = process.env.BOT_TOKEN;
+
+// Reemplaza 'YOUR_BOT_TOKEN' con tu token de bot
+const bot = new Telegraf(token);
+
 bot.start((ctx) => {
-    ctx.reply(`Hello! Welcome to Axolt Miner 😎
+  ctx.reply(
+    `Hello! Welcome to Axolt Miner 😎
 You are now the director of a crypto exchange.
 Which one? You choose. Tap the screen, collect coins, pump up your passive income,
 develop your own income strategy.
 We’ll definitely appreciate your efforts once the token is listed (the dates are coming soon).
-Don't forget about your friends — bring them to the game and get even more coins together!`);
+Don't forget about your friends — bring them to the game and get even more coins together!`,
+    Markup.inlineKeyboard([
+      [Markup.button.url("Let´s Play!!!", app)],
+      [Markup.button.callback("Button 2", "btn_2")],
+      [Markup.button.callback("Button 3", "btn_3")],
+    ])
+  );
 });
 
-bot.on(message('text'), async (ctx) => {
-    // Explicit usage
-    await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-  
-    // Using context shortcut
-    await ctx.reply(`Hello ${ctx.state.role}`)
-  })
+bot.action("btn_2", (ctx) => {
+  ctx.reply("You pressed Button 2");
+});
+
+bot.action("btn_3", (ctx) => {
+  ctx.reply("You pressed Button 3");
+});
+
+
 bot.launch();
+
+console.log("Bot is running...");
